a01751d5da7d60613d2e6914aad60670105d3f06
Eric Choi <meetcds@foxmail.com>
2023-11-08 19:34:49 +0800

    fix: fix the user store

--- src/components/UsePiniaDemo.vue
+++ src/components/UsePiniaDemo.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>{{ userStore.name }}</div>
+    <div>{{ userStore.userInfo.userName }}</div>
     <n-button strong secondary type="primary" @click="handleClick">
         更改姓名
     </n-button>
@@ -11,6 +11,19 @@ import { useUserStore } from '@/store/user'
 const userStore = useUserStore()
 
 const handleClick = () => {
-    userStore.updateName('李四')
+    userStore.setUserInfo({
+        /**用户 Id，关联 user_info 表的主键 */
+        id: '1',
+        /**登录类型 */
+        identityType: 1,
+        /**标识（手机号、邮箱、用户名或第三方应用的唯一标识） */
+        identifier: '123',
+        /**用户名（唯一） */
+        userName: 'testUser',
+        /**昵称 */
+        nickName: 'testUser',
+        /**头像 URL */
+        avatar: '1'
+    })
 }
 </script>
